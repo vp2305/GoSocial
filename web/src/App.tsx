ee44055925b4357474051302bbf73fd428d0058f
Vaibhav Patel <vaibhav.p2305@gmail.com>
2024-11-30 21:37:18 -0500

    feat(cors): Implemented confirm token and cors configuration.

---
+++ web/src/App.tsx
@@ -0,0 +1,25 @@
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from "react";
+import "./App.css";
+import ConfirmPage from "./ConfirmPage";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div className="App-header">
+              <h1>Welcome to GoSocialMedia!</h1>
+            </div>
+          }
+        />
+        <Route path="/confirm/:token" element={<ConfirmPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ web/src/ConfirmPage.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+function ConfirmPage() {
+  const { token } = useParams();
+
+  const handleConfirm = async () => {
+    if (!token) {
+      return;
+    }
+
+    const response = await fetch(
+      `http://localhost:8080/v1/users/activate/${token}`,
+      {
+        method: "PUT",
+      }
+    );
+
+    if (response.ok) {
+      alert("Email confirmed successfully!");
+    } else {
+      alert("Failed to confirm email.");
+    }
+  };
+
+  return (
+    <div
+      className="w-auto min-h-screen p-5 text-center justify-center items-center flex flex-col gap-4"
+      style={{ backgroundColor: "#282c34", color: "white" }}
+    >
+      <h1 className=" text-5xl ">Confirm your email</h1>
+      <p className="text-xl">
+        Once you confirm your email, you will be able to log in to your account
+        and start using GoSocialMedia.
+      </p>
+
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={handleConfirm}
+      >
+        Confirm
+      </button>
+    </div>
+  );
+}
+
+export default ConfirmPage;
