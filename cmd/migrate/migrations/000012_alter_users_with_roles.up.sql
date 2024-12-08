2ec2a303fb9f0adbdfe8995844165ed3da6f84b2
Vaibhav Patel <vaibhav.p2305@gmail.com>
2024-12-08 13:16:23 +0530

    feat(authorization): Database setup for authorization and role based access control

---
+++ cmd/migrate/migrations/000012_alter_users_with_roles.up.sql
@@ -0,0 +1,18 @@
+ALTER TABLE IF EXISTS users
+ADD COLUMN role_id INT REFERENCES roles(id) DEFAULT 1;
+
+
+UPDATE users
+SET role_id = (
+  SELECT id
+  FROM roles
+  WHERE name = 'user'
+)
+WHERE role_id IS NULL;
+
+
+ALTER TABLE users
+ALTER COLUMN role_id DROP DEFAULT;
+
+ALTER TABLE users
+ALTER COLUMN role_id SET NOT NULL;
