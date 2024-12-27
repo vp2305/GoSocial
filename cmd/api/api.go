d2ee1322d0849557bf65e351b2eb34331cbaa437
Vaibhav Patel <vaibhav.p2305@gmail.com>
2024-12-27 12:08:47 +0530

    feat(server): Implemented graceful shutdown for scaling and good practice

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -6,8 +6,13 @@ import (
 	"SocialMedia/internal/ratelimiter"
 	"SocialMedia/internal/store"
 	"SocialMedia/internal/store/cache"
+	"context"
+	"errors"
 	"fmt"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/go-chi/chi/v5"
@@ -174,8 +179,38 @@ func (app *application) run(mux http.Handler) error {
 		IdleTimeout:  time.Minute,
 	}
 
+	// ? Graceful shutdown implementation
+	shutdown := make(chan error)
+
+	go func() {
+		quit := make(chan os.Signal, 1)
+
+		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+		s := <-quit
+
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		app.logger.Infow("signal caught", "signal", s.String())
+
+		shutdown <- srv.Shutdown(ctx)
+	}()
+
 	app.logger.Infow("server has started", "addr", app.config.addr, "env", app.config.env)
 	app.logger.Infow("swagger initiated", "url", docs.SwaggerInfo.Host+"/v1/swagger/")
 
-	return srv.ListenAndServe()
+	err := srv.ListenAndServe()
+	if !errors.Is(err, http.ErrServerClosed) {
+		return err
+	}
+
+	err = <-shutdown
+	if err != nil {
+		return err
+	}
+
+	app.logger.Infow("server has stopped", "addr", app.config.addr, "env", app.config.env)
+
+	return nil
 }
